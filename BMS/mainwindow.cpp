46b7638bfd15a3035b250a9964d5d4455083dbf6
baotinn <82329882+baotinn@users.noreply.github.com>
2021-12-10 17:29:19 +0700

    Update UI Login

--- BMS/mainwindow.cpp
+++ BMS/mainwindow.cpp
@@ -20,15 +20,16 @@ void MainWindow::on_Submit_clicked()
    user=ui->lineEdit_2->text();
    pass=ui->lineEdit->text();
    qDebug()<<user<<pass;
-   QFile file("D:/account.txt");
+   QFile file("D:/BMS/account.txt");
    file.open(QIODevice::ReadOnly);
    QTextStream in(&file);
    int f=0;
    while(!in.atEnd()){
-       QString saved_user=in.readLine();
-       QString saved_pass=in.readLine();
-       if(QString::compare(user,saved_user)==0&&QString::compare(pass,saved_pass)==0){
-           QMessageBox::information(this,"Login success.", "You have successfully logged in!");
+       QString saved_account=in.readLine();
+       QStringList info = saved_account.split(";");
+       //QString saved_pass=in.readLine();
+       if(QString::compare(user,info[0])==0 && QString::compare(pass,info[1])==0){
+           //QMessageBox::information(this,"Login success.", "You have successfully logged in!");
            f=1;
            ui->stackedWidget->setCurrentIndex(1);
            break;
